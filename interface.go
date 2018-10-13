d68cdc00c5ac0afa5183fd7c8436395c2bbcda28
Benjamin Estes <benjamin.lee.estes@gmail.com>
2018-10-12 20:56:18 -0700

    Expand godoc introduction.

--- interface.go
+++ interface.go
@@ -1,6 +1,26 @@
 // Package robots implements robots.txt file parsing and matching
 // based on Google's specification. Read the full specification at:
 // https://developers.google.com/search/reference/robots_txt.
+//
+// This specification assumes crawling a path is allowed by
+// default. For both user agent and path matching, the longest match
+// wins. For exampke, "/path" has precedence over "/". Path matches
+// are case-sensitive; user agent matches are not. Acceptable
+// metacharacters in paths specified by allow/disallow rules are "*" and
+// "$". "*" matches anything, and "$" matches the end of a path. Metacharacters
+// are not used in user agent matching. As a special case, a user agent
+// pattern "*" matches any user agent.
+//
+// A generous parser is specified. Any line representing valid input
+// is used, any invalid line is silently discarded. This is true even
+// if the content parsed is in some unexpected format, like HTML.
+//
+// The rules for choosing which robots.txt file applies to a given URL
+// are well-defined, and somewhat complex. Use Locate to get the URL
+// of a robots.txt file that applies to a specific URL, and to any
+// other URL whose crawling would be governed by that robots.txt file.
+// It is the caller's responsibility to make sure you use the correct
+// Robots object for a path; with Locate this should be easy.
 package robots
 
 import (
